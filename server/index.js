313b40586e6609a2dbb7da970410496fb3afdfc6
Kamal Walia <kamalw1709@gmail.com>
2022-10-21 21:23:24 +0530

    JWT Token

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
+dotenv.config();
+
+console.log('KW101', process.env.PORT)
 const PORT = process.env.PORT || 3010
 
 const app = express();
@@ -37,6 +42,28 @@ app.get('/greetings', (req, res) => {
 
 app.post('/greetings2', (req, res) => {
     res.json({message: 'Hello From Server'});
+});
+
+app.post('/generateJWT', (req, res) => {
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userName: 'Kamal'
+    }
+    const token = jwt.sign(data, jwtSecretKey)
+    res.send(token);
+});
+
+app.get('/verifyToken', (req, res) => {
+    let tokenHeader = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    try {
+        const headerKey = req.header(tokenHeader)
+        const isTokenValid  = jwt.verify(headerKey, jwtSecretKey);
+        return isTokenValid ? res.send('Verified') : res.status(401).send("Error") 
+    } catch (err) {
+        return res.status(401).send("Error" , err) 
+    }
 });
 
 app.listen(PORT, () => {
